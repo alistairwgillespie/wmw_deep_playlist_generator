580ba16054d7bc178a4b2221e99ac128f660e0de
EC2 Default User <ec2-user@ip-172-16-41-115.ap-southeast-2.compute.internal>
2020-02-26 04:53:23 +0000

    Added model strawman files

---
+++ model/LstmEstimator.py
@@ -0,0 +1,43 @@
+# torch imports
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+## TODO: Complete this classifier
+class LstmEstimator(nn.Module):
+    """
+    LSTM Estimator for generating sequential-based track target variables.
+    """
+
+    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
+    def __init__(self, input_features, embedding_dim, output_dim):
+        """s
+        Initialize the model by setting up linear layers.
+        Use the input parameters to help define the layers of your model.
+        :param input_features: the number of input features in your training/test data
+        :param hidden_dim: helps define the number of nodes in the hidden layer(s)
+        :param output_dim: the number of outputs you want to produce
+        """
+        super(LstmEstimator, self).__init__()
+        
+        # The LSTM takes track features as inputs, and outputs hidden states
+        # with dimensionality hidden_dim
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        
+        self.hidden2target = nn.Linear(hidden_dim, output_dim)
+        
+    
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, input_track):
+        """
+        Perform a forward pass of our model on input features, x.
+        :param input_track: A batch of input features of size (batch_size, input_features)
+        :return: A single, sigmoid-activated value as output
+        """
+        
+        # define the feedforward behavior
+        
+        lstm_out, _ = self.lstm(embeds.view(len(input_track), 1, -1))
+        target_feat = self.hidden2tag(lstm_out.view(len(input_track), -1))
+        
+        return target_feat
---
+++ model/RnnEstimator.py
@@ -0,0 +1,23 @@
+# torch imports
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+## TODO: Complete this RNN Estimator
+class RNN(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        
+    
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        return output, hidden
